611b0887c92a7a41bdd4cab35f0cb8b90da45b2e
00Voldemort <guessingwallah@gmail.com>
2025-09-22 15:21:57 +0530

    Add Dockerfiles for Render deployment

--- generator/src/main/java/com/resume/generator/controller/ResumeController.java
+++ generator/src/main/java/com/resume/generator/controller/ResumeController.java
@@ -20,7 +20,7 @@ import java.util.zip.ZipEntry;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"https://resume-generator-forntend.onrender.com", "http://localhost:3000"})
 public class ResumeController {
 
     @Autowired
